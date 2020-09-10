0352c2fe9080a1751a27ce09f139f4e0cad56f1c
Max <pro_max_shevchenko@mail.ru>
2020-09-10 21:11:37 +0300

    4_Task

---
+++ 1_Laba/4_Task.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    freopen("smallsort.in", "r", stdin);
+    freopen("smallsort.out", "w", stdout);
+
+    /* Ввод данных */
+    int n, i, j;
+    cin >> n;
+    int a[n];
+
+    for (i = 0; i < n; i++) {
+        cin >> a[i];
+    }
+
+    /* Сортировка данных */
+    for (i = 0; i < n; i++) {
+
+        int a_min = i;
+
+        for (j = i + 1; j < n; j++) {
+
+            if (a[j] < a[a_min]) {
+                a_min = j;
+            }
+
+        }
+
+        swap(a[i], a[a_min]);
+
+    }
+
+    /* Вывод данных */
+    for (i = 0; i < n; i++) {
+        cout << a[i] << " ";
+    }
+
+
+    return 0;
+}
