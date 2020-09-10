372280bff6bc659c8c2f46d33afb58721c0321e8
Max <pro_max_shevchenko@mail.ru>
2020-09-10 12:13:05 +0300

    Task_2

--- 1_Laba/2_Task.cpp
+++ 1_Laba/2_Task.cpp
@@ -2,10 +2,11 @@
 using namespace std;
 
 int main() {
-    int a, b;
+    long long a;
+    long long b;
     freopen("aplusbb.in", "r", stdin);
     freopen("aplusbb.out", "w", stdout);
     cin >> a >> b;
     cout << a + b*b << endl;
-    return 0;
+  return 0;
 }
