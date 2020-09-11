6cfd7a8d61e38bcb73cc3f1d50e0f5f107a0aa90
Max <pro_max_shevchenko@mail.ru>
2020-09-11 11:18:53 +0300

    5_Task

--- 1_Laba/5_Task.cpp
+++ 1_Laba/5_Task.cpp
@@ -3,41 +3,48 @@ using namespace std;
 
 int main() {
 
-    freopen("smallsort.in", "r", stdin);
-    freopen("smallsort.out", "w", stdout);
+    // freopen("smallsort.in", "r", stdin);
+    // freopen("smallsort.out", "w", stdout);
 
     /* Ввод данных */
     int n, i, j;
     cin >> n;
-    int a[n];
-    
+    float a[n];
+    float k;
+    float min_k = 1000001;
+    float max_k = 0;
 
     for (i = 0; i < n; i++) {
         cin >> a[i];
     }
 
     /* Сортировка данных */
+    /* Поиск минимума */
     for (i = 0; i < n; i++) {
-
-        int a_min = i;
-
-        for (j = i + 1; j < n; j++) {
-
-            if (a[j] < a[a_min]) {
-                a_min = j;
-            }
-
+        if (a[i] < min_k) {
+            min_k = a[i];
         }
+    }
 
-        swap(a[i], a[a_min]);
+    /* Поиск среднего */
+    for (i = 0; i < n; i++) {
+        k += a[i];
+    }
+    k / n;
 
+    /* Поиск максимума */
+    for (i = 0; i < n; i++) {
+        if (a[i] > max_k) {
+            max_k = a[i];
+        }
     }
 
     /* Вывод данных */
-    for (i = 0; i < n; i++) {
-        cout << a[i] << " ";
-    }
+    // for (i = 0; i < n; i++) {
+    //     cout << a[i] << " ";
+    // }
 
+    cout << min_k << " " << max_k << endl;
 
     return 0;
 }
