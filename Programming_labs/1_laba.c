93501f35ef9adc5368d86ec7b3d4cfc0e0a05ecc
Max <pro_max_shevchenko@mail.ru>
2020-09-17 16:59:17 +0300

    Folders_Update+Programming labs

---
+++ Programming_labs/1_laba.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+    int a, c;
+    // ввод данных
+    scanf("%d", &a);
+    printf("%d\n", a);
+    // вывод данных
+    scanf("%c", &c);
+    printf("%c\n", c);
+}
