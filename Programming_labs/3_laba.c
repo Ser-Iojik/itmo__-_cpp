73e6245b4695569201c1ac78dd8ce22e1584e48f
Max <pro_max_shevchenko@mail.ru>
2020-09-19 11:55:27 +0300

    4 laba 2 task

--- Programming_labs/3_laba.c
+++ Programming_labs/3_laba.c
@@ -10,23 +10,30 @@
 
 int main() {
     // Task 1
+    // Ввели a в 8-ой сс
     int a;
     scanf("%o", &a);
 
     // Task 2
-    int b;
-    scanf("%d", &b);
-    printf("%d\n", b);
+    // Вывели a в 10-ой сс
+    printf("%d\n", a);
 
     // Task 3
+    // Вывели a в 16-ой сс
+    // И вывели с побитовым сдвигом вправо на 2
     printf("%x\n", a);
+    // a = a << 2;
     printf("%x\n", a << 2);
 
     // Task 4
+    // Вывели число в 16-ой сс
+    // Вывели число с побитовым отрицанием
+    printf("%x\n", a);
     printf("%x\n", ~a);
 
     // Task 5
-    int c;
-    scanf("%x", &c);
-    printf("%x %x\n", c ^ a);
+    int b;
+    // Ввели число в 16-ой сс
+    scanf("%x", &b);
+    printf("%x", b ^ a);
 }
